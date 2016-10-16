0c968d3d603fb17300408f26a80eb68c03e81110
Harley Trung <harley@bsa.edu.vn>
2016-10-16 20:48:25 +0700

    implement login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
    @user = User.new user_params
    if @user.save
      flash[:success] = "User created successfully"
+     # OPTIONAL: sign the user in without logging in
+     # OPTIONAL: email the user
      redirect_to root_path
    else
      render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create]
+
+  delete 'logout' => 'sessions#destroy'
   resources :users
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
