0664a2f6e21a3e85c35a539d40e759a2d4044206
Harley Trung <harley@bsa.edu.vn>
2016-10-24 08:38:18 +0700

    add sign_in user in controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
   describe "GET /users" do
     it "redirects if user is not logged in" do
       get :index
@@ -8,10 +12,10 @@ RSpec.describe UsersController, type: :controller do
     end
 
     it "displays users if user is signed in" do
-      # user = User.create! email: "a@example.com", password: "asdf"
-      # get :index
-      # expect(response).to render_template("index")
-      # expect(response).to have_http_status(200)
+      user = User.create! email: "a@example.com", password: "asdf"
+      sign_in user
+      get :index
+      expect(response).to have_http_status(200)
     end
   end
 end
